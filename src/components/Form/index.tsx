29c066099c564cd94a95a98e7a7c770bd2292a52
Adão Ribeiro <adao-eduardo@hotmail.com>
2023-01-25 17:45:20 -0300

    card credit validator

--- src/components/Form/index.tsx
+++ src/components/Form/index.tsx
@@ -2,7 +2,7 @@ import { useContext, useState } from "react"
 import { MyContext } from "../../context/mycontext";
 import { ICardFront } from "../cardFront"
 import styles from "./style.module.scss"
-
+import validator from 'validator'
 export function Form() {
     const { infoCards, setInfoCards }: any = useContext(MyContext);
 
@@ -11,17 +11,69 @@ export function Form() {
         dateerror: false,
         cvcerror: false
     });
+
+    function validateCreditCard() {
+        const formatNumber = /^[0-9]+$/;
+        setError((prev) => {
+            const data = { ...prev, numberError: false };
+            return data
+        });
+        setError((prev) => {
+            const data = { ...prev, dateerror: false };
+            return data
+        });
+        setError((prev) => {
+            const data = { ...prev, cvcerror: false };
+            return data
+        });
+        if (!validator.isCreditCard(infoCards.cardnumber.toString())) {
+            setError((prev) => {
+                const data = { ...prev, numberError: true };
+                return data
+            });
+
+        }
+        if (infoCards.cardmes == "" || infoCards.cardano == "") {
+            setError((prev) => {
+                const data = { ...prev, dateerror: true };
+                return data
+            });
+        }
+        if (infoCards.cardcvc == "") {
+            setError((prev) => {
+                const data = { ...prev, cvcerror: true };
+                return data
+            });
+        }
+    }
+
     const handleInput = (event) => {
         const { name, value } = event.target;
+        var newValue = value;
+        if (name == "cardnumber") {
+            newValue = cc_format(value);
+        }
         const altered = (prev) => {
-            const data = { ...prev, [name]: value };
+            const data = { ...prev, [name]: newValue };
             return data
         };
         setInfoCards(altered);
     }
-    const handleSubmit = () => {
+    const handleSubmit = (event) => {
         console.log(infoCards)
+        event.preventDefault();
     }
+    function cc_format(value: string) {
+        const v = value
+            .replace(/\s+/g, "")
+            .substring(0, 16);
+        const parts = [];
+        for (let i = 0; i < v.length; i += 4) {
+            parts.push(v.substring(i, i + 4));
+        }
+        return parts.length > 1 ? parts.join(" ") : value;
+    }
+
     return (
         <form className={styles.form} onSubmit={handleSubmit}>
             <div className={styles.containerCardHolder}>
@@ -30,7 +82,7 @@ export function Form() {
             </div>
             <div className={styles.containerCardHolder}>
                 <label>Card Number</label>
-                <input value={infoCards.cardnumber} onChange={handleInput} placeholder="e.g. 1234 5678 9123 0000" type="text" name="cardnumber" />
+                <input maxLength={19} value={infoCards.cardnumber} onChange={handleInput} placeholder="e.g. 1234 5678 9123 0000" type="text" name="cardnumber" />
                 {error.numberError &&
                     <p>Wrong format, number only</p>
                 }
@@ -44,6 +96,7 @@ export function Form() {
                             <input value={infoCards.cardmes} onChange={handleInput} placeholder="MM" type="text" name="cardmes" />
                             <input value={infoCards.cardano} onChange={handleInput} placeholder="YY" type="text" name="cardano" />
                         </div>
+
                         {error.dateerror &&
                             <p>Can't be blank</p>
                         }
@@ -58,7 +111,7 @@ export function Form() {
                     </div>
                 </div>
             </div>
-            <button>Confirm</button>
+            <button onClick={validateCreditCard}>Confirm</button>
         </form>
     )
 }
